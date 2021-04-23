6263ae8f1135c977c411374bc5381e72ddd420e4
Sahil Mishra <sahilmishra0012@outlook.com>
2021-04-23 08:57:43 +0530

    Integrated Latest Prescription Support with Image Viewer

--- src/app/components/testList/TestList.jsx
+++ src/app/components/testList/TestList.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './testList.css';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 import MuiTableCell from "@material-ui/core/TableCell";
-import { Button, Chip, Collapse, createMuiTheme, Grid, IconButton, Table, TableBody, TableContainer, TableHead, TableRow, ThemeProvider, Typography } from '@material-ui/core';
+import { Button, Chip, Collapse, createMuiTheme, Grid, IconButton, Table, TableBody, TableContainer, TableHead, TableRow, ThemeProvider, Typography, TextField } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
@@ -31,16 +31,13 @@ const darkTheme = createMuiTheme({
     }
 });
   
-function createData(name, type, date, priority, doctor, radiologist, image, note) {
+function createData(test, type, file, note) {
     return {
-        name,
+        test,
         type,
-        date,
-        priority,
-        doctor,
-        radiologist,
-        image,
+        file,
         note
+
     };
 }
 
@@ -49,6 +46,8 @@ function Row(props) {
     const [open, setOpen] = React.useState(false);
     const [color, setColor] = React.useState('#202427');
     const classes = useRowStyles();
+    const middleNameRef = React.useRef();
+
 
     function handleCollapse() {
         setOpen(!open);
@@ -67,13 +66,9 @@ function Row(props) {
                     </IconButton>
                 </TableCell>
                 <TableCell component="th" scope="row">
-                    {row.name}
+                    {row.test}
                 </TableCell>
                 <TableCell align="left">{row.type}</TableCell>
-                <TableCell align="left">{row.date}</TableCell>
-                <TableCell align="left"><Chip color="secondary" size="small" label={row.priority} /></TableCell>
-                <TableCell align="left">{row.doctor}</TableCell>
-                <TableCell align="left">{row.radiologist}</TableCell>
             </TableRow>
             <TableRow hover>
                 <TableCell style={{ paddingBottom: 0, paddingTop: 0, backgroundColor: color }} colSpan={7}>
@@ -81,7 +76,7 @@ function Row(props) {
                         <Box margin={1}>
                             <Grid container spacing={2}>
                                 <Grid container item xs={8}>
-                                    <img src={row.image} style={{width: '95%', height: '20rem'}}/>
+                                    <img src={URL.createObjectURL(row.file)} style={{width: '90%', height: '90%'}}/>
                                 </Grid>
                                 <Grid container item xs={4} spacing={2}>
                                     <Grid container item xs={12}>
@@ -90,7 +85,7 @@ function Row(props) {
                                     <Grid container item xs={12}>
                                         <Typography variant="h4"><b>0.88</b></Typography>
                                     </Grid>
-                                    <Grid container item xs={12} spacing={1}>
+                                    {/* <Grid container item xs={12} spacing={1}>
                                         <Grid container item xs={4}>
                                             <Button variant="contained" style={{backgroundColor: "#515151", color: "white"}}>
                                                 Confirm
@@ -101,13 +96,23 @@ function Row(props) {
                                                 Edit
                                             </Button>
                                         </Grid>
-                                    </Grid>
+                                    </Grid> */}
+                                    
                                     <Grid container item xs={12}>
-                                        <Button variant="contained" color="primary">Select for analysis</Button>
+                                        <TextField
+                                            multiline
+                                            rows={10}
+                                        variant="outlined"
+                                        fullWidth
+                                        label="Report"
+                                        size="small"
+                                        inputRef={middleNameRef}
+                                    />
+                                        {/* Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries */}
                                     </Grid>
-                                    <Grid container item xs={12}>
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
-                                    </Grid>
+                                    {/* <Grid container item xs={12}>
+                                        <Button variant="contained" color="primary">Save</Button>
+                                    </Grid> */}
                                 </Grid>
                             </Grid>
                         </Box>
@@ -121,26 +126,23 @@ function Row(props) {
 Row.propTypes = {
     row: PropTypes.shape({
         type: PropTypes.string.isRequired,
-        doctor: PropTypes.string.isRequired,
-        date: PropTypes.string.isRequired,
-        image: PropTypes.arrayOf(PropTypes.string.isRequired),
-        name: PropTypes.string.isRequired,
-        radiologist: PropTypes.string.isRequired,
-        priority: PropTypes.number.isRequired,
+        file: PropTypes.arrayOf(PropTypes.string.isRequired),
+        test: PropTypes.string.isRequired,
         note: PropTypes.string
     }).isRequired,
 };
   
-const rows = [
-    createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 34, 'A. Sagachyo', 'N. Rao', ['https://www.ucsf.edu/sites/default/files/fields/field_insert_file/news/brain%20scan.jpg']),
-    createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 3, 'A. Sagachyo', 'N. Rao', ['https://www.ucsf.edu/sites/default/files/fields/field_insert_file/news/brain%20scan.jpg']),
-    createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 3, 'A. Sagachyo', 'N. Rao', ['https://www.ucsf.edu/sites/default/files/fields/field_insert_file/news/brain%20scan.jpg']),
-];
+    // createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 34, 'A. Sagachyo', 'N. Rao', ['C:\\fakepath\\output-onlinepngtools.png']),
+    // createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 3, 'A. Sagachyo', 'N. Rao', ['https://www.ucsf.edu/sites/default/files/fields/field_insert_file/news/brain%20scan.jpg']),
+    // createData('Blood Test', 'Monomgraphy', 'Jun 1 11:34 am', 3, 'A. Sagachyo', 'N. Rao', ['https://www.ucsf.edu/sites/default/files/fields/field_insert_file/news/brain%20scan.jpg']),
   
 export default function TestList() {
+    const location = useLocation();
+    const rows = location.state.data;
+    console.log(rows)
 
     function handleLogout() {
-
+        console.log(rows)
     }
 
     return (
@@ -184,15 +186,12 @@ export default function TestList() {
                                         <TableCell />
                                         <TableCell>Name</TableCell>
                                         <TableCell align="left">Type</TableCell>
-                                        <TableCell align="left">Date</TableCell>
-                                        <TableCell align="left">Priority</TableCell>
-                                        <TableCell align="left">Medical Doctor</TableCell>
-                                        <TableCell align="left">Radiologist</TableCell>
                                     </TableRow>
                                     </TableHead>
                                     <TableBody>
-                                    {rows.map((row) => (
-                                        <Row key={row.name} row={row} />
+                                        {rows.map((row) => (
+                                            console.log(row.file),
+                                        <Row key={row.test} row={row} />
                                     ))}
                                     </TableBody>
                                 </Table>
