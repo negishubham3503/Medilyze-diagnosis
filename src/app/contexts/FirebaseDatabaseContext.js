7c9318d644e7bea86d3a72059525332de70258e7
Sahil Mishra <sahilmishra0012@outlook.com>
2021-04-27 22:55:55 +0530

    Created function to add reports and scans

--- src/app/contexts/FirebaseDatabaseContext.js
+++ src/app/contexts/FirebaseDatabaseContext.js
@@ -32,6 +32,33 @@ export const getPrescription = async (value) => {
     }
 }
 
+export const addReportURL = async (value, url, report) => {
+    const pidRef = db.collection('patientIDUID').doc(value.trim());
+    const uidDoc = await pidRef.get()
+    if (uidDoc.exists) {
+        let data = uidDoc.data()['uid'];
+        let name = await rdbms.ref('prescription/' + data).once("value", snapshot => {
+        }
+        );
+        var len = name.val().length;
+        if (name.val().reverse()[0][Object.keys(name.val().reverse()[0])][0]['Reports'])
+        {
+            var reports = name.val().reverse()[0][Object.keys(name.val().reverse()[0])][0]['Reports']
+        }
+        else {
+            var reports = []
+        }
+        reports.push({'url': url,'report': report})
+        await rdbms.ref('prescription/'+ data + "/"+(len-1) +"/"+ Object.keys(name.val().reverse()[0])+"/"+0).update({
+            Reports: reports,
+        });
+    }
+    else {
+        console.log("No such patient!");
+        return -999;
+    }
+}
+
 export const getAppointments = async (value) => {
     const pidRef = db.collection('patientIDUID').doc(value.trim());
     const uidDoc = await pidRef.get()
